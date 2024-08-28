1d9dfcae05ace195a730ed0777cf1b3fb3fb132a
Tarjei H. S. <tarjeihs@hotmail.com>
2024-08-28 17:09:51 +0200

    Added Stopwatch utility class

---
+++ Engine/Source/Utils/Stopwatch.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+
+#include "Core/Logger.h"
+
+#define STOPWATCH(DebugName) SStopwatch Stopwatch(##DebugName)
+
+class SStopwatch
+{
+public:
+    using Clock = std::chrono::high_resolution_clock;
+    using TimePoint = std::chrono::time_point<Clock>;
+
+    explicit SStopwatch(const char* InDebugName)
+        : StartTime(Clock::now()), DebugName(InDebugName)
+    {
+    }
+
+    ~SStopwatch()
+    {
+        const auto EndTime = Clock::now();
+        const long DurationS = std::chrono::duration_cast<std::chrono::seconds>(EndTime - StartTime).count();
+        const long DurationMS = std::chrono::duration_cast<std::chrono::milliseconds>(EndTime - StartTime).count();
+        const long DurationNS = std::chrono::duration_cast<std::chrono::nanoseconds>(EndTime - StartTime).count();
+        RK_LOG_TRACE("({}) Elapsed time: {}s:{}ms:{}ns", DebugName, DurationS, DurationMS, DurationNS);
+    }
+
+private:
+    TimePoint StartTime;
+
+    const char* DebugName;
+};
